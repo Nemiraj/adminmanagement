299c804bdf3dc1b1c2968c4cd7258ec067c5ae61
Nemiraj <yuvanemiraj@gmail.com>
2026-04-15 23:00:40 +0530

    Add root route

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 
-const API_BASE = process.env.REACT_APP_API_URL || '/api';
+const API_BASE = process.env.REACT_APP_API_URL || 'https://adminmanagementbackend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE,
